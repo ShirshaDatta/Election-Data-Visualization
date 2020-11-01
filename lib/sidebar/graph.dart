dfce40c8f3472a9b96199c95611a36187aedf1ad
Shirsha Datta <shirshadatta2000@gmail.com>
2020-11-02 03:19:42 +0530

    graphs updated

--- lib/sidebar/graph.dart
+++ lib/sidebar/graph.dart
@@ -6,18 +6,60 @@ class Graph1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Data Visualization"),
-      ),
+      appBar: AppBar(title: Text("Data Visualization"),),
      // backgroundColor: Colors.black12,
-      body: SingleChildScrollView(
+      body: SingleChildScrollView( 
               child: Container(
+                padding: EdgeInsets.only(top:20),
           child: Column(children: [
+           
             Image.asset('assets/graph1.PNG'),
-            Image.asset('assets/graph2.PNG'),
-            Image.asset('assets/graph3.PNG'),
-            Image.asset('assets/graph4.PNG'),
-            //Image.asset('assets/graph2.PNG'),
-            
+             Container(
+               color: Colors.greenAccent,
+               child: Text("2019 Pie chart",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+             ),
+           
+            Image.asset('assets/plot_zoom1.PNG'),
+             Container(
+               color: Colors.greenAccent,
+               child: Text("State vs Total no of votes Scatter Plot",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+             ),
+            Image.asset('assets/plot_zoom2.PNG'),
+             Container(
+               color: Colors.greenAccent,
+               child: Text("State vs Total no of votes Jitter Plot",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+             ),
+            Image.asset('assets/newplot3.PNG'),
+            Image.asset('assets/newplot4.PNG'),
+            Image.asset('assets/plot_zoom_png5.PNG'),
+            Container(
+               color: Colors.greenAccent,
+               child: Text("Election results vs Total no of votes ",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+             ),
+            Image.asset('assets/plot_zoom_png6.PNG'),
+           Container(
+               color: Colors.greenAccent,
+               child: Text("Histogram for % of votes ",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+             ), 
+            Image.asset('assets/Capture7_1.PNG'),
+            Image.asset('assets/Capture8.PNG'),
+            Image.asset('assets/Capture9.PNG'),
+            Container(
+               color: Colors.greenAccent,
+               child: Text("EVM Votes ",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+             ),
           ],),
      ),
       ),
